73d14e6a9a6fa51e05700ea59f1942d980e9e288
UmenoKoudai <106291399+UmenoKoudai@users.noreply.github.com>
2023-06-05 19:04:42 +0900

    初コミット

---
+++ Assets/ItemData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DataBase", menuName = "ItemData")]
+public class ItemData : ScriptableObject
+{
+
+}
