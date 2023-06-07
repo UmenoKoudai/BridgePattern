5b8df7b262b3ee3c8bdd35f7184eb3041b4d5c2c
UmenoKoudai <106291399+UmenoKoudai@users.noreply.github.com>
2023-06-08 08:30:08 +0900

    Save機能実装中

--- Assets/Sccript/Inventory/Inventory.cs
+++ Assets/Sccript/Inventory/Inventory.cs
@@ -8,6 +8,7 @@ public class Inventory : InstanceSystem<Inventory>
     [SerializeField] GameObject _inventory;
     ItemState[] _myItems;
     int _itemSlotCount;
+    int[] _getItemIndex;
 
     public ItemState[] MyItems { get => _myItems; set => _myItems = value; }
     public int ItemSlotCount { get => _itemSlotCount; }
@@ -16,6 +17,7 @@ public class Inventory : InstanceSystem<Inventory>
     {
         _itemSlotCount = _inventory.transform.childCount;
         _myItems = Enumerable.Repeat(new ItemState(), ItemSlotCount).ToArray();
+        Load();
         SetItem();
         //_inventory.SetActive(false);
     }
@@ -29,6 +31,52 @@ public class Inventory : InstanceSystem<Inventory>
             if (_myItems[i].ItemID != -1)
             {
                 viewItem.MyItem = _myItems[i];
+            }
+            Save();
+        }
+    }
+
+    void Save()
+    {
+        _getItemIndex = new int[_myItems.Length * 2];
+        int j = 0;
+        for(int i = 0; i < _myItems.Length; i++)
+        {
+            if (_myItems[i].ItemID != -1)
+            {
+                _getItemIndex[j] = _myItems[i].ItemID;
+                j++;
+                _getItemIndex[j] = _myItems[i].ItemCount;
+                j++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        _getItemIndex.OnSave();
+    }
+
+    void Load()
+    {
+        int[] loadIndex = _getItemIndex.OnLoad();
+        if (loadIndex != null)
+        {
+            int j = 0;
+            for (int i = 0; i < loadIndex.Length / 2; i++)
+            {
+                if (loadIndex[j] != -1)
+                {
+                    _myItems[i] = Resources.Load<ItemData>("ItemBase").Item[loadIndex[j] - 1];
+                    j++;
+                    _myItems[i].ItemCount = loadIndex[j];
+                    j++;
+                }
+
+                else
+                {
+                    break;
+                }
             }
         }
     }
